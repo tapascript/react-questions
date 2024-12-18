4a3cff542be9db463cfa0f72dd4cdba057350b8f
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-12-18 15:44:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
+      <p className="bg-gray-300 p-2 rounded text-3xl">
+        <a 
+          href="https://www.youtube.com/watch?v=hiiGUjEkzbM&list=PLIJrr73KDmRw-T8bdJn3XxVMbH-zlooKb" 
+          target="_blank"
+          className="underline mx-1">Learn React 19 With Code</a>
+        <span>▶️</span>  
+      </p>
+    </div>
+  );
+}
+
+export default App;
